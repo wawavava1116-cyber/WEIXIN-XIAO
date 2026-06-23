de1362a76675d7fe6f3ae154abe06cb9cbfbadef
wawavava1116-cyber <wawavava1116@gmail.com>
2026-06-23 13:36:47 +0800

    更新世界杯赛程预测与复盘归档

--- utils/buildInfo.js
+++ utils/buildInfo.js
@@ -1,5 +1,5 @@
 const DATABASE_VERSION = '1.1.0'
-const DATABASE_UPDATED_AT = new Date('2026-06-22T14:57:00+08:00')
+const DATABASE_UPDATED_AT = new Date('2026-06-23T13:32:00+08:00')
 
 function pad(value) {
   return String(value).padStart(2, '0')
